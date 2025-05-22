31ca62f88afa47cc40841ef7daf2db68e1ea2c9e
Joyel <0018joyelvft@gmail.com>
2025-05-22 20:24:20 +0530

    update: pages updated

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -8,11 +8,15 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency = '$'
-const delivery_fee = 10
+    const delivery_fee = 10;
+
 const [search,setSearch] = useState('')
 const [showSearch,setShowSearch] = useState(false)
+
+
     const value = {
-        products,currency,delivery_fee,search,setSearch,showSearch,setShowSearch  
+        products,currency,delivery_fee,
+        search,setSearch,showSearch,setShowSearch  
     }
 
     return(
