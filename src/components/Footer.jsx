45bfeb5e46bc7b6ed2beb5b0f18b9d530982bd75
Joyel <0018joyelvft@gmail.com>
2025-05-22 18:16:18 +0530

    update:page updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,6 +12,27 @@ const Footer = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio rem dolore corporis eius nemo? Laudantium numquam, alias ab perspiciatis eos enim, officiis libero quae aut aliquam laborum, nulla tenetur iure.
             </p>
         </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+1-212-456-7890</li>
+                <li>contact@foreveryou.com</li>
+            </ul> 
+        </div>
+      </div>
+      <div>
+        <hr/>
+        <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
       </div>
     </div>
   )
