5b1c9870e70dccf7eab41660c18e971e698bca73
Joyel <0018joyelvft@gmail.com>
2025-05-23 22:10:21 +0530

    updated: items added

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import React from "react";
 
@@ -9,18 +9,43 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
     const currency = '$'
     const delivery_fee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
+    const [carItems, setCartItems] = useState({});
 
-const [search,setSearch] = useState('')
-const [showSearch,setShowSearch] = useState(false)
+    const addToCart = async (itemId, size) => {
+
+        let cartData = structuredClone(carItems);
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    useEffect(() => {
+        console.log(carItems)
+    }, [carItems])
 
 
     const value = {
-        products,currency,delivery_fee,
-        search,setSearch,showSearch,setShowSearch  
+        products, currency, delivery_fee,
+        search, setSearch, showSearch, setShowSearch,
+        carItems, addToCart
     }
 
-    return(
-        <ShopContext.Provider value={value}> 
+    return (
+        <ShopContext.Provider value={value}>
             {props.children}
         </ShopContext.Provider>
     )
