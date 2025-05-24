1daeff7a11970fef47d4ef492123d13c30499e68
Joyel <0018joyelvft@gmail.com>
2025-05-24 15:39:27 +0530

    cart updated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
+import { assets } from '../assets/assets';
 
 function Cart() {
 
@@ -43,11 +44,14 @@ function Cart() {
                   <img  className='w-16 sm:w-20'src={productData.image[0]} alt="" />
                   <div>
                     <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
-                    <div className='flexitems-center gap-5 mt-2'>
-
+                    <div className='flex items-center gap-5 mt-2'>
+                         <p>{currency}{productData.price}</p>
+                         <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
                     </div>
                   </div>
                 </div>
+                <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity}/>
+                <img className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
               </div>
             )
 
