05cfdbf6a0bc4590b026cd1b3a3a6a2531714212
Joyel <0018joyelvft@gmail.com>
2025-05-26 13:48:00 +0530

    update:contact updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsLetterBox'
 
 function Contact() {
   return (
@@ -13,9 +14,15 @@ function Contact() {
         <div className='my-10 flex flex-col justify-center md:flex-row  gap-10 mb-28'>
            <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
            <div className='flex flex-col justify-center items-start gap-6'>
-
+                 <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+                 <p className='text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
+                 <p className='text-gray-500'>Tel: (+91) 7510817400 <br /> Email:amin@forever.com</p>
+                 <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+                 <p className='text-gray-500'>Learn more about our team and job openings.</p>
+                 <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
            </div>
         </div>
+        <NewsletterBox />
     </div>
   )
 }
